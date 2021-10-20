6b7c5d40c8eb3ef889871f8749102f5754d6709e
Mustafa Caliskan <mustafa.clsknn@gmail.com>
2021-10-20 12:50:18 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,12 @@
+console.log("Merhaba")
+let dolarDun = 9.20
+let dolarBugun = 9.30
+{
+    let dolarDun = 9.10
+}
+let konutKredileri = ["Konut kredisi","Emlak Konut Kredisi", "Kamu Konut Kredisi"]
+console.log("<ul>")
+for(let i=0; i<konutKredileri.length;i++){
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
